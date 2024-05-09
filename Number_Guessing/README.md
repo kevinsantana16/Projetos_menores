5eb839b9d66e2470c3a72913acb8b4d295ce04c0
Kevin Santana <99555227+kevinsantana16@users.noreply.github.com>
2024-05-09 15:17:45 -0300

    adicionei Number-Guessing

---
+++ Number_Guessing/README.md
@@ -0,0 +1,5 @@
+# Number-Guessing
+
+Jogo: Adivinhe o número 
+
+O usuário tem 5 tentativas para adivinhar os números 1 e 15 que são gerados aleatoriamente 
---
+++ Number_Guessing/nu.py
@@ -0,0 +1,33 @@
+from random import randint
+
+print("Jogo: adivinhe o número")
+print("Você tem 5 tentativas")
+
+tentativas = 0 
+acertos = 0
+erros = 0
+
+while tentativas != 5:
+    num = randint(1, 15)
+    user = int(input("Escolha um número entre 1 e 15: "))
+
+    if num == user:
+        print("Número correto!!!")
+        print(f"Número gerado: {num}")
+        print(f"Número que você escolheu: {user}")
+        tentativas += 1
+        acertos += 1
+        erros += 1
+        print("#"*45) 
+    
+    else:
+        print("Número Incorreto")
+        print(f"Número gerado: {num}")
+        print(f"Número que você escolheu: {user}")
+        tentativas += 1
+        acertos += 1
+        erros += 1
+        print("#"*45)
+print("Jogo finalizado")
+print(f"Acertos: {acertos}; Erros: {erros}")
+
